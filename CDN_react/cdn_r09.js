48478ae7e40013e812eea2850bbf150c49f41ac6
BSEom <ebs970725@gmail.com>
2025-06-09 17:22:16 +0900

    first commit

---
+++ CDN_react/cdn_r09.js
@@ -0,0 +1,7 @@
+const users = [
+    {id:1, title: '제목들1', name: 'John', date: '2025-05-05', count: '23'},
+    {id:2, title: '제목들2', name: 'Tom', date: '2025-02-15', count: '123'},
+    {id:3, title: '제목들3', name: 'Smith', date: '2023-07-06', count: '213'},
+    {id:4, title: '제목들4', name: 'Bill', date: '2021-06-31', count: '231'},
+    {id:5, title: '제목들5', name: 'Steve', date: '2020-10-09', count: '230'},
+]
