c760381e811a9e7676f37c13173a4e3d4613ff29
BSEom <ebs970725@gmail.com>
2025-06-12 17:29:01 +0900

    업데이트

--- VITE_react/vite_02/src/App.jsx
+++ VITE_react/vite_02/src/App.jsx
@@ -5,20 +5,39 @@ import './App.css'
 import Home from './pages/Home'
 import Css from './pages/Ex07'
 import TodoList from './pages/Ex08'
+import Eff from './pages/Ex09'
+import FetchData from './pages/Ex10'
+import UseM from './pages/Ex11'
+import Table from './pages/Ex12'
+import Ball from './pages/Ex13'
+import Axi from './pages/Ex14'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
       <Link to="/">Home</Link> | {" "}
       <Link to="/ex07">예제7</Link> | {" "}
       <Link to="/ex08">예제8</Link> | {" "}
+      <Link to="/ex09">예제9</Link> | {" "}
+      <Link to="/ex10">예제10</Link> | {" "}
+      <Link to="/ex11">예제11</Link> | {" "}
+      <Link to="/ex12">예제12</Link> | {" "}
+      <Link to="/ex13">예제13</Link> | {" "}
+      <Link to="/ex14">예제14</Link> | {" "}
 
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/ex07' element={<Css />} />
         <Route path='/ex08' element={<TodoList />} />
+        <Route path='/ex09' element={<Eff />} />
+        <Route path='/ex10' element={<FetchData />} />
+        <Route path='/ex11' element={<UseM />} />
+        <Route path='/ex12' element={<Table />} />
+        <Route path='/ex13' element={<Ball />} />
+        <Route path='/ex14' element={<Axi />} />
+
       </Routes>
     </>
   )
--- VITE_react/vite_02/src/pages/Ex08.jsx
+++ VITE_react/vite_02/src/pages/Ex08.jsx
@@ -1,29 +1,48 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 const TodoList = () => {
     const [indata, setIndata] = useState("")
     const [arr, setArr] = useState([])
+   
+    const [ckdata, setCkdata] = useState({indata: false})
 
     const handleInput = (e) => {
         setIndata(e.target.value)
     }
     const handleAdd = () => {
         setArr([...arr, indata])
+        setIndata("")
+        console.log(arr,"!")
+    }
+
+    const handelPress = (e) => {
+        if (e.key=="Enter")
+        {
+            handleAdd()
+        }
+    }
+
+    // const handelCk = (e) => {
+    //     console.log(e.target.id, e.target.value, e.target.checked)
+    // }
+
+    const delcl = () => {
+
     }
 
     return(
         <>
             <h1>8. 어레이를 이용한 ToDoList</h1>
             <label htmlFor="inin">배열요소 입력 : </label>
-            <input type="text" id="inin" onChange={handleInput} value={indata}/>{" "}
+            <input type="text" id="inin" onChange={handleInput} onKeyDown={handelPress} value={indata}/>{" "}
             <button onClick={handleAdd}>추가</button>
-            <button disabled={!(arr.length > 0)}>삭제</button>
+            <button onClick={delcl} disabled={!(arr.length > 0)}>삭제</button>
             <div>{indata}</div>
             <hr />
             {/* <div>{arr}</div> */}
             {arr.map((v, i) => {
                 return(
-                    <div key={i}>{`${i+1}: ${v}`}</div>
+                    <div id={i+1} key={i} ><input type="checkbox" id={i+1} value={v} onChange={handelCk}/>{`${i+1}: ${v}`}</div>
                 )
             })}
         </>
